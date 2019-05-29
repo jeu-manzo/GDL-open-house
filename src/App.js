dc0d3893ef36a1a4ce6f608a2b90844d7b3bf873
Melisa Quesada <melisa.quc@gmail.com>
2019-05-29 14:03:34 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
-import StudentsList from './components/studentsList';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './routerComponents/Home';
+import Attendance from './routerComponents/Attendance';
+import Lists from './routerComponents/List';
+import Login from './routerComponents/Login';
+import Err from './routerComponents/Err';
 
 function App() {
-  return (
-    <div>
-      <StudentsList />
-    </div>
-  );
+    return(
+      <BrowserRouter>
+        <div>
+            <Switch>
+              <Route path='/' component={Home} exact/>
+              <Route path='/attendance' component={Attendance} />
+              <Route path='/lists' component={Lists} />
+              <Route path='/login' component={Login} />
+              <Route component={Err} />
+            </Switch>
+        </div>
+      </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/routerComponents/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lists = () => {
+    return(
+        <div>
+            <p>Lists</p>
+        </div>
+    )
+}
+
+export default Lists;
