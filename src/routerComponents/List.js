61e9ea127f2f2d4cadf71f33d3b25b19dc46fd9b
Melisa Quesada <melisa.quc@gmail.com>
2019-05-29 17:56:57 -0500

    calendar (not working)

--- src/routerComponents/List.js
+++ src/routerComponents/List.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Calendar from '../components/Calendar';
 
 const Lists = () => {
     return(
         <div>
-            <p>Lists</p>
+            <Calendar />
         </div>
     )
 }
