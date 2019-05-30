a4fe0a235c2a19af0d6feeede80608e62805015c
Ily Trevino <ily.trevino@live.com>
2019-05-30 17:32:42 -0500

    Basic design

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,26 +3,26 @@ import firebase from '../config/FirestoreConfig';
 import Students from '../data/students.json';
 
 export default class Table extends Component {
-    render() {
-        return(
-            <table>
-                <thead>
-                    <tr>
-                        <th>Estudiante</th>
-                        <th>Hora</th>
-                        <th>Estatus</th>
-                        <th>Nota</th>
-                    </tr>
-                    {Students.filter(student => {
-                        return student.role === 'student'
-                    }).map(student => {
-                        return <tr key={student.id}><td>{student.name}</td></tr>
-                    })}
-                    {/* {firebase.firestore().collection('users').onSnapshot(snapshot => {
-                        console.log(snapshot)
+  render() {
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>Estudiante</th>
+            <th>Hora</th>
+            <th>Estatus</th>
+            <th>Nota</th>
+          </tr>
+          {Students.filter(student => {
+              return student.role === 'student'
+          }).map(student => {
+              return <tr key={student.id}><td>{student.name}</td></tr>
+          })}
+          {/* {firebase.firestore().collection('users').onSnapshot(snapshot => {
+              console.log(snapshot)
 ;                    })} */}
-                </thead>
-            </table>
-        )
-    }
+        </thead>
+      </table>
+    )
+  }
 }
--- src/routerComponents/Attendance.js
+++ src/routerComponents/Attendance.js
@@ -2,14 +2,23 @@ import React from 'react';
 import AttendanceHeader from '../components/AttendanceHeader';
 import Qr from '../components/Qr';
 import Navigation from './Navigation';
+import '../styles/Summary.css';
+import '../styles/Navigation.css';
 
 
 const Attendance = () => {
   return(
-    <div className="attendance">
-      <Navigation />
-      <AttendanceHeader />
-      <Qr />
+    <div>
+      <div className="summary">
+        <Navigation className="navigation"/>
+        <h1 className="summary__header" />
+      </div>
+      <div>
+        <div className="summary__blank">
+          <AttendanceHeader />
+          <Qr />
+        </div>
+      </div>
     </div>
   )
 }
