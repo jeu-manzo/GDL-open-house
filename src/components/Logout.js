66b12dcf6501c5d3bb3521a19aa202662a4b69bd
Lourdes Jeunette Manzo Ramírez <jeunettemanzo@gmail.com>
2019-05-30 15:04:50 -0500

    add changes

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import firebase from '../config/FirestoreConfig';
 import { NavLink } from 'react-router-dom';
 
-export default class Logount extends React.Component {
+export default class Logout extends React.Component {
     logout= () => {
         firebase.auth().signOut();
     }
